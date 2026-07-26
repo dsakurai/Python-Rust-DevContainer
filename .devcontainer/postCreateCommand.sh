d06e7be0e3a6e93732ef8d4fe06ddf83d2bb72ee
Daisuke Sakurai <d.sakurai0@gmail.com>
2026-07-26 10:54:41 +0900

    Initial commit

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Create virtual environment in .venv
+uv venv --allow-existing .venv
+
+uv sync
---
+++ README.md
@@ -0,0 +1,1 @@
+A minimalist template for using Python and Jupyter on DevContainer with uv.
